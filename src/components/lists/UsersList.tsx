94b38ead012bffb209ca6a2a0cbbdb5174fef531
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2024-01-15 11:57:56 +0330

    Update schema and add user list component (generated by blackbox.ai )

--- src/components/lists/UsersList.tsx
+++ src/components/lists/UsersList.tsx
@@ -2,6 +2,8 @@ import { Admin, User } from "@prisma/client";
 import Image from "next/image";
 import React from "react";
 import blue from "@/public/images/default-blue.png";
+import { TicketIcon } from "@heroicons/react/24/outline";
+import { BiCross } from "react-icons/bi";
 
 const UsersList = ({ data = [], title }: { data: User[]; title: string }) => {
   return (
@@ -12,7 +14,7 @@ const UsersList = ({ data = [], title }: { data: User[]; title: string }) => {
       <div className="flex flex-col items-start justify-start gap-2 min-w-full">
         {data.map((user) => {
           return (
-            <div key={user.id}>
+            <div key={user.id} className="min-w-full flex items-center justify-evenly gap-5 text-white" >
               <span className="flex items-center gap-2  capitalize text-white">
                 <Image
                   src={user.image || blue.src}
@@ -23,6 +25,11 @@ const UsersList = ({ data = [], title }: { data: User[]; title: string }) => {
                 />
                 {user.name}
               </span>
+              <span>@{user.name}</span>
+              <span>{user.email}</span>
+              <span>{user.hasNotification ? <TicketIcon /> : <BiCross />}</span>
+              <span className="">{user.id}</span>
+              <span className="">{user.IsAdmin ? "admin" : "user"}</span>
             </div>
           );
         })}
